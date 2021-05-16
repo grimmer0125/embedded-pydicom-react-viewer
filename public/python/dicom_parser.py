32db5dccee431a37bb8eddd6f62f946e09042d96
Grimmer <grimmer0125@gmail.com>
2021-05-16 23:16:11 +0800

    Add comments and no truncate some trailing bytes for compressed DICOM

--- public/python/dicom_parser.py
+++ public/python/dicom_parser.py
@@ -56,10 +56,18 @@ def get_manufacturer_independent_pixel_image2d_array(ds):
             print("multi frame")
             j2k_precision, j2k_sign = None, None
             # multiple compressed frames
+            # working case (50):
+            # 1. 0002.dcm, some are [-5], [-4], [-6]
+            # 2. color3d_jpeg_baseline , some frames needs [-1] but some do not need
             frame_count = 0
             for frame in decode_data_sequence(ds.PixelData):
                 frame_count += 1
                 print(f"frame i:{frame_count}, len:{len(frame)}")
+                # a = frame[0]
+                # b = frame[1]
+                # c = frame[len(frame)-2]
+                # d = frame[len(frame)-1]
+                # print(f"{a},{b},{c},{d}")
                 if frame_count == 1:
                     pixel_data = frame
                 # im = _decompress_single_frame(
@@ -75,14 +83,14 @@ def get_manufacturer_independent_pixel_image2d_array(ds):
                 #     params = get_j2k_parameters(frame)
                 #     j2k_precision = params.setdefault("precision", ds.BitsStored)
                 #     j2k_sign = params.setdefault("is_signed", None)
-            # TODO: what is the rule of -5, and is it always ?
-            p2 = pixel_data[:-5]
+            # TODO: what is the rule of -5/-1? But even not using pixel_data[:-1], pixel_data[:-5], still work
+            p2 = pixel_data
         else:
             print("single frame")
+            # working case:
+            # JPGLosslessP14SV1_1s_1f_8b.dcm, 70
             pixel_data = defragment_data(ds.PixelData)
-            # TODO: 1. in some file, its ending is [-1], why?
-            #       2. does
-            p2 = pixel_data  # [:-1]
+            p2 = pixel_data
         print(f"pixel_data:{len(pixel_data)}")
         # return None, pixel_data
 
