410d6519a75ecdec8753af374abcf03fcafa956e
grimmer <grimmer0125@gmail.com>
2021-08-22 17:07:25 +0800

    Format the code and update react-dropzone and add File typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useRef, useEffect, useState } from 'react'
+import { useRef, useEffect, useState, useCallback } from "react";
 
-import Dropzone from "react-dropzone";
+import Dropzone, { useDropzone } from "react-dropzone";
 import { loadDicomAsync } from "./utility";
-import {initPyodide, parseByPython} from "./pyodideHelper"
+import { initPyodide, parseByPython } from "./pyodideHelper";
 const { fromEvent } = require("file-selector");
-
 
 const dropZoneStyle = {
   borderWidth: 2,
@@ -18,7 +17,6 @@ const dropZoneStyle = {
 const MAX_WIDTH_SERIES_MODE = 400;
 const MAX_HEIGHT_SERIES_MODE = 400;
 
-
 function App() {
   const myCanvasRef = useRef<HTMLCanvasElement>(null);
   const myImg = useRef<HTMLImageElement>(null);
@@ -26,44 +24,38 @@ function App() {
   const [isPyodideLoading, setPyodideLoading] = useState(true);
 
   useEffect(() => {
-    async function init(){
-      console.log("initialize Pyodide, python browser runtime")
+    async function init() {
+      console.log("initialize Pyodide, python browser runtime");
       await initPyodide(); // do some initialization
       setPyodideLoading(false);
-      console.log("finish initializing Pyodide")
+      console.log("finish initializing Pyodide");
     }
     init();
   }, []); // [] means only 1 time, if no [], means every update this will be called
 
-  const onDropFiles = async (acceptedFiles: any[]) => {
-    if (acceptedFiles.length > 0) {
-      acceptedFiles.sort((a, b) => {
-        return a.name.localeCompare(b.name);
-      });
-
-      loadFile(acceptedFiles[0]);
+  const loadFile = async (file: File) => {
+    const buffer = await loadDicomAsync(file);
+    // NOTE: besides get return value (python code last line expression),
+    // python data can be retrieved by accessing python global object:
+    // pyodide.globals.get("image")<-dev version (but stable v0.17.0a2 can use), pyodide.pyimport('sys')<-stable version;
+    console.log("start to use python to parse parse dicom data");
+    const { compressedData, data, width, height } = await parseByPython(buffer);
+    console.log("parsing dicom done");
+    if (compressedData) {
+      console.log("render compressedData");
+      renderFrameByPythonCompressedData(compressedData, width, height);
+    } else {
+      console.log("render incompressedData");
+      renderFrameByPythonData(data, width, height);
     }
   };
 
-  const loadFile = async (file: any) => {
-    const buffer = await loadDicomAsync(file);
-    // NOTE: besides get return value (python code last line expression), 
-    // python data can be retrieved by accessing python global object:
-    // pyodide.globals.get("image")<-dev version (but stable v0.17.0a2 can use), pyodide.pyimport('sys')<-stable version; 
-    console.log("start to use python to parse parse dicom data")
-    const { compressedData, data, width, height } = await parseByPython(buffer);
-    console.log('parsing dicom done')
-    if (compressedData){
-      console.log("render compressedData")
-      renderFrameByPythonCompressedData(compressedData, width, height)
-    } else {
-      console.log("render incompressedData")
-      renderFrameByPythonData(data, width, height);
-    }
-  }
-
-  const renderFrameByPythonCompressedData = async (imageUnit8Array: Uint8Array, rawDataWidth: number, rawDataHeight: number) => {
-    console.log("renderFrameByPythonCompressedData")
+  const renderFrameByPythonCompressedData = async (
+    imageUnit8Array: Uint8Array,
+    rawDataWidth: number,
+    rawDataHeight: number
+  ) => {
+    console.log("renderFrameByPythonCompressedData");
     const canvasRef = myCanvasRef;
     if (!canvasRef.current) {
       console.log("canvasRef is not ready, return");
@@ -82,18 +74,26 @@ function App() {
     // needs arrayBuffe, but feed unit8array
     const buffer = imageUnit8Array.buffer;
     // 1000000
-    //  718940 
+    //  718940
     // 75366400
     // JPEG Lossless
 
     // works for myImg.current.src
     // https://stackoverflow.com/questions/37228285/uint8array-to-arraybuffer
     function typedArrayToBuffer(array: Uint8Array): ArrayBuffer {
-      return array.buffer.slice(array.byteOffset, array.byteLength + array.byteOffset)
+      return array.buffer.slice(
+        array.byteOffset,
+        array.byteLength + array.byteOffset
+      );
     }
-    const buffer2  = typedArrayToBuffer(imageUnit8Array)
-    console.log("len:",imageUnit8Array.length, buffer.byteLength, buffer2.byteLength);
-    const blob = new Blob([buffer2], {type: "image/jpeg"});
+    const buffer2 = typedArrayToBuffer(imageUnit8Array);
+    console.log(
+      "len:",
+      imageUnit8Array.length,
+      buffer.byteLength,
+      buffer2.byteLength
+    ); //   718924, 75366400 (buffer.byteLength is more than actual size), 718924
+    const blob = new Blob([buffer2], { type: "image/jpeg" });
     const url = URL.createObjectURL(blob);
 
     // works for myImg.current.src
@@ -107,30 +107,42 @@ function App() {
     // var base64 = window.btoa(data);
     // const url2 =  "data:image/jpeg;base64," + base64;
 
-    // works => become not work 
-    const my_svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg"></svg>`; 
-    const my_svg_blob = new Blob([my_svg], { type: 'image/svg+xml;charset=utf-8' })
-    const url4 = URL.createObjectURL(my_svg_blob)
+    // works => become not work
+    const my_svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg"></svg>`;
+    const my_svg_blob = new Blob([my_svg], {
+      type: "image/svg+xml;charset=utf-8",
+    });
+    const url4 = URL.createObjectURL(my_svg_blob);
 
     // works for myImg.current.src
-    const content = new Uint8Array([137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 5, 0, 0, 0, 5, 8, 6, 0, 0, 0, 141, 111, 38, 229, 0, 0, 0, 28, 73, 68, 65, 84, 8, 215, 99, 248, 255, 255, 63, 195, 127, 6, 32, 5, 195, 32, 18, 132, 208, 49, 241, 130, 88, 205, 4, 0, 14, 245, 53, 203, 209, 142, 14, 31, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130]);
+    const content = new Uint8Array([
+      137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 5,
+      0, 0, 0, 5, 8, 6, 0, 0, 0, 141, 111, 38, 229, 0, 0, 0, 28, 73, 68, 65, 84,
+      8, 215, 99, 248, 255, 255, 63, 195, 127, 6, 32, 5, 195, 32, 18, 132, 208,
+      49, 241, 130, 88, 205, 4, 0, 14, 245, 53, 203, 209, 142, 14, 31, 0, 0, 0,
+      0, 73, 69, 78, 68, 174, 66, 96, 130,
+    ]);
     const url3 = URL.createObjectURL(
-      new Blob([content.buffer], { type: 'image/png' } /* (1) */)
+      new Blob([content.buffer], { type: "image/png" } /* (1) */)
     );
 
     if (myImg) {
-      myImg.onload = function() {
+      myImg.onload = function () {
         /// draw image to canvas
-        console.log("on load:", myImg.width, myImg.height)
+        console.log("on load:", myImg.width, myImg.height);
         c.width = myImg.width;
         c.height = myImg.height;
-        ctx.drawImage((myImg as any), 0,0, myImg.width, myImg.height);
+        ctx.drawImage(myImg as any, 0, 0, myImg.width, myImg.height);
       };
       myImg.src = url; //"https://raw.githubusercontent.com/grimmer0125/grimmer0125.github.io/master/images/bio.png";
     }
-  }
+  };
 
-  const renderFrameByPythonData = async (imageUnit8Array: Uint8ClampedArray, rawDataWidth: number, rawDataHeight: number) => {
+  const renderFrameByPythonData = async (
+    imageUnit8Array: Uint8ClampedArray,
+    rawDataWidth: number,
+    rawDataHeight: number
+  ) => {
     const canvasRef = myCanvasRef;
     if (!canvasRef.current) {
       console.log("canvasRef is not ready, return");
@@ -146,25 +158,48 @@ function App() {
       return;
     }
 
-    // no allocate new memory 
+    // no allocate new memory
     const imgData = new ImageData(imageUnit8Array, rawDataWidth, rawDataHeight);
     ctx.putImageData(imgData, 0, 0);
-  }
+  };
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    console.log("acceptedFiles");
+
+    if (acceptedFiles.length > 0) {
+      acceptedFiles.sort((a: any, b: any) => {
+        return a.name.localeCompare(b.name);
+      });
+      loadFile(acceptedFiles[0]);
+    }
+
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
     <div className="flex-container">
       <div>
         <div className="flex-container">
           <div>
-            DICOM Image Viewer {isPyodideLoading ? ", loading python runtime" : ""}
+            DICOM Image Viewer{" "}
+            {isPyodideLoading ? ", loading python runtime" : ""}
           </div>
         </div>
         <div>
           <div className="flex-container">
-            <Dropzone
-              preventDropOnDocument={false}
-              style={dropZoneStyle}
-              getDataTransferItems={(evt) => fromEvent(evt)}
+            <div style={dropZoneStyle} {...getRootProps()}>
+              <input {...getInputProps()} />
+              {isDragActive ? (
+                <p>Drop the files here ...</p>
+              ) : (
+                <p>Drag 'n' drop some files here, or click to select files</p>
+              )}
+            </div>
+
+            {/* <Dropzone
+              // style={dropZoneStyle}
+              // getDataTransferItems={(evt) => fromEvent(evt)}
               onDrop={onDropFiles}
             >
               <div
@@ -180,18 +215,12 @@ function App() {
                   </p>
                 </div>
               </div>
-            </Dropzone>
+            </Dropzone> */}
           </div>
         </div>
-        <div
-          className="flex-container"
-        >
-          <div
-            className="flex-column-justify-align-center"
-          >
-            <div
-              className="flex-column_align-center"
-            >
+        <div className="flex-container">
+          <div className="flex-column-justify-align-center">
+            <div className="flex-column_align-center">
               {/* <img style={{width:500, height:250}} ref={myImg} /> */}
               <canvas
                 ref={myCanvasRef}
--- src/utility.ts
+++ src/utility.ts
@@ -1,15 +1,14 @@
-export async function loadDicomAsync(file: any): Promise<ArrayBuffer> {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = () => {
-        const fileContent = reader.result;
-        resolve(fileContent as ArrayBuffer);
-      };
-      reader.onabort = () => console.log("file reading was aborted");
-      reader.onerror = () => console.log("file reading has failed");
-      reader.readAsArrayBuffer(file);
-    });
-  }
-  
-  export default {};
-  
+export async function loadDicomAsync(file: File): Promise<ArrayBuffer> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const fileContent = reader.result;
+      resolve(fileContent as ArrayBuffer);
+    };
+    reader.onabort = () => console.log("file reading was aborted");
+    reader.onerror = () => console.log("file reading has failed");
+    reader.readAsArrayBuffer(file);
+  });
+}
+
+export default {};
