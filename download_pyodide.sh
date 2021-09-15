0b7b5a0a57d61fe99012200e95ec13894e1f5ada
Grimmer <grimmer0125@gmail.com>
2021-09-15 09:53:45 +0800

    Update pyodide zip url

--- download_pyodide.sh
+++ download_pyodide.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BASEDIR=$(dirname "$0")
-curl -JL https://github.com/grimmer0125/embedded-pydicom-react-viewer/releases/download/untagged-4c8410552dae8d87d56b/pyodide.zip > $BASEDIR/pyodide.zip
+curl -JL https://github.com/grimmer0125/embedded-pydicom-react-viewer/releases/download/untagged-77e55ea5908aaad294e1/pyodide.zip > $BASEDIR/pyodide.zip
 unzip $BASEDIR/pyodide.zip -d $BASEDIR/public
 rm -r $BASEDIR//public/__MACOSX
 rm $BASEDIR/pyodide.zip
