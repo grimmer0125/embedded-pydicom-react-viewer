6ba682e0c4cb3f4126f9e5874ae8a5a55f7c8030
grimmer <grimmer0125@gmail.com>
2021-09-26 14:20:15 +0800

    Try to release unused memory used in decoding jpeg and top dicomObj.current

--- src/jpegDecoder.ts
+++ src/jpegDecoder.ts
@@ -5,20 +5,24 @@ const JpxImage = require('./jpx');
 const JpegLSDecoder = require('./jpeg-ls');
 
 // borrow from https://github.com/rii-mango/daikon/blob/master/src/image.js#L706
-const decompressJPEG = (jpg: any, transferSyntaxUID: string, bitsAllocated: number, isBytesInteger: boolean) => {
+const decompressJPEG = (buffer: any, transferSyntaxUID: string, bitsAllocated: number, isBytesInteger: boolean) => {
   // 1. PyProxyClass 
   // 2. Uint8Array (if use pyodide.to_js first)
   // 3. PyProxyClass (is use pyodide.create_proxy)
+
+  buffer = buffer.getBuffer()
+
   if (transferSyntaxUID === "1.2.840.10008.1.2.4.57" || transferSyntaxUID === "1.2.840.10008.1.2.4.70") {
     // JPEGLossless
     // console.log("jpg:", jpg, typeof jpg)
     const decoder = new jpeg.lossless.Decoder();
-    const decoded = decoder.decode(jpg);
+    const decoded = decoder.decode(buffer.data);
+    buffer.release()
     return decoded.buffer
   } else if (transferSyntaxUID === "1.2.840.10008.1.2.4.50" || transferSyntaxUID === "1.2.840.10008.1.2.4.51") { // 50, 51
     // JPEGBaseline
     const decoder = new JpegDecoder();
-    decoder.parse(new Uint8Array(jpg));
+    decoder.parse(new Uint8Array(buffer.data));
     const width = decoder.width;
     const height = decoder.height;
 
@@ -28,20 +32,22 @@ const decompressJPEG = (jpg: any, transferSyntaxUID: string, bitsAllocated: numb
     } else if (bitsAllocated === 16) {
       decoded = decoder.getData16(width, height);
     }
-    const buffer = decoded.buffer;
-    return buffer;
+    buffer.release()
+    return decoded.buffer;
   } else if (transferSyntaxUID === "1.2.840.10008.1.2.4.90" || transferSyntaxUID === "1.2.840.10008.1.2.4.91") {
     // JPEG_2000
     // remaining not tested yet: "1.2.840.10008.1.2.4.90"
 
     const decoder = new JpxImage();
-    decoder.parse(new Uint8Array(jpg));
+    decoder.parse(new Uint8Array(buffer.data));
     const decoded = decoder.tiles[0].items;
+    buffer.release()
     return decoded.buffer;
   } else if (transferSyntaxUID === "1.2.840.10008.1.2.4.80" || transferSyntaxUID === "1.2.840.10008.1.2.4.81") {
     // JPEGLS
     const decoder = new JpegLSDecoder();
-    const decoded = decoder.decodeJPEGLS(new Uint8Array(jpg), isBytesInteger);
+    const decoded = decoder.decodeJPEGLS(new Uint8Array(buffer.data), isBytesInteger);
+    buffer.release()
     return decoded.pixelData.buffer;
   }
 
